494999cf92ad873b1d276923cb82a6c2bc5c9d96
sharmasuper <mohit6375sharma@gmail.com>
2024-03-05 05:14:30 +0530

    form validation

--- src/Components/Students/EditStudents.jsx
+++ src/Components/Students/EditStudents.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import {useForm} from 'react-hook-form' 
 import {DevTool} from '@hookform/devtools'
+let renderCount = 0;
+
+ 
+
 function EditStudents() {
     const form = useForm()
-    const {register,control} = form
+    const {register,control,handleSubmit} = form
+
+   const onsubmit = (data) =>{
+    console.log(data)
+   }
+
+  renderCount++
    
+ 
+
   return (
     <div>
-    <form>
+        <h1>EditStudents - ({renderCount/2})</h1>
+    <form onSubmit={handleSubmit(onsubmit)} noValidate>
         <label htmlFor='username'>UserName</label>
-        <input type='text' id='username' name='username' {...register("username")} /><br/>
+        <input type='text' id='username' {...register("username",{required :"Username is required" })} />
+       <br/>
             <pre/>
         <label htmlFor='email'>E-mail</label>
-        <input type='email' id='channel' name='channel'{...register("email")} /><br/>
+        <input type='email' id='channel' name='channel' {...register("email")} /><br/>
        <pre/>
         <button >Submit</button>
 
