107a38fd912d1badc04517000793f8437078f79f
sharmasuper <mohit6375sharma@gmail.com>
2024-03-05 05:40:50 +0530

    form validation

--- src/Components/Students/EditStudents.jsx
+++ src/Components/Students/EditStudents.jsx
@@ -3,7 +3,7 @@ import {useForm} from 'react-hook-form'
 import {DevTool} from '@hookform/devtools'
 let renderCount = 0;
 
- 
+ ///pattern validation
 
 function EditStudents() {
     const form = useForm()
@@ -22,11 +22,18 @@ function EditStudents() {
         <h1>EditStudents - ({renderCount/2})</h1>
     <form onSubmit={handleSubmit(onsubmit)} noValidate>
         <label htmlFor='username'>UserName</label>
-        <input type='text' id='username' {...register("username",{required :"Username is required" })} />
-       <br/>
+        <input type='text' id='username' {...register("username",{required :"Username is required" })} /> 
+       <br/>  
             <pre/>
         <label htmlFor='email'>E-mail</label>
-        <input type='email' id='channel' name='channel' {...register("email")} /><br/>
+        <input type='email' id='channel' name='channel'
+         {...register("email",{
+            pattern : {
+                value : /ms6375349671@gmail.com/,
+                message : "invalid"
+
+            },
+         })} /><br/>
        <pre/>
         <button >Submit</button>
 
